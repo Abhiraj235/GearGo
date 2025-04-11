f500c4dc53622d312e087bf2e045d4ec57a3e58d
Abhiraj-5 <hiraj8445@gmail.com>
2025-04-11 22:44:00 +0530

    Added

--- app/(admin)/admin/page.jsx
+++ app/(admin)/admin/page.jsx
@@ -2,8 +2,8 @@ import { getDashboardData } from "@/actions/admin";
 import { Dashboard } from "./_components/dashboard";
 
 export const metadata = {
-  title: "Dashboard | Vehiql Admin",
-  description: "Admin dashboard for Vehiql car marketplace",
+  title: "Dashboard | GearGoAdmin",
+  description: "Admin dashboard for GearGocar marketplace",
 };
 
 export default async function AdminDashboardPage() {
