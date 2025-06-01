848fb0970c83ca6362f22b7714bc10279b7e9823
Abhiraj-235 <abhishekrajwar8445@gmail.com>
2025-06-01 19:59:47 +0530

    update

--- middleware.js
+++ middleware.js
@@ -7,18 +7,14 @@ const isProtectedRoute = createRouteMatcher([
   "/saved-cars(.*)",
   "/reservations(.*)",
 ]);
-
-// Create Arcjet middleware
 const aj = arcjet({
   key: process.env.ARCJET_KEY,
-  // characteristics: ["userId"], // Track based on Clerk userId
   rules: [
-    // Shield protection for content and security
     shield({
       mode: "LIVE",
     }),
     detectBot({
-      mode: "LIVE", // will block requests. Use "DRY_RUN" to log only
+      mode: "LIVE", 
       allow: [
         "CATEGORY:SEARCH_ENGINE", // Google, Bing, etc
         // See the full list at https://arcjet.com/bot-list
